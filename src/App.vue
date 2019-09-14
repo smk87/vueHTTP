4336199fa81a13d48112436217660c3086067285
smk87 <sqlnmhmdkhanemon@gmail.com>
2019-09-14 13:18:06 +0600

    POST,GET request added.

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,67 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Http</h1>
-            </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Http</h1>
+        <div class="form-group">
+          <label>Username</label>
+          <input type="text" class="form-control" v-model="user.username" />
         </div>
+        <div class="form-group">
+          <label>Mail</label>
+          <input type="text" class="form-control" v-model="user.mail" />
+        </div>
+        <button class="btn btn-primary" @click="submit">Submit</button>
+        <hr />
+        <button class="btn btn-primary" @click="fetchData">Get Data</button>
+        <ul class="list-grou">
+          <li class="list-group-item" v-for="u in users">
+            {{ u.username }} - {{ u.mail }}
+          </li>
+        </ul>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
+export default {
+  data() {
+    return {
+      user: {
+        username: "",
+        mail: ""
+      },
+      users: []
+    };
+  },
+  methods: {
+    submit() {
+      this.$http
+        .post("https://vuehttp-cc0f9.firebaseio.com/data.json", this.user)
+        .then(
+          res => {
+            console.log(res);
+          },
+          err => {
+            console.log(err);
+          }
+        );
+    },
+    fetchData() {
+      this.$http
+        .get("https://vuehttp-cc0f9.firebaseio.com/data.json")
+        .then(res => res.json())
+        .then(data => {
+          const resultArray = [];
+          for (let key in data) {
+            resultArray.push(data[key]);
+          }
+          this.users = resultArray;
+        });
     }
+  }
+};
 </script>
 
 <style>
